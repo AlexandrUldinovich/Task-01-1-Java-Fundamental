32850fe960619bef253f15cd71df800bfa69e9c6
AlexandrUldinovich <heathblacke@gmail.com>
2022-02-16 09:33:08 +0300

    Second Commit

--- sixthtask/thirdtask/TaskView.java
+++
@@ -1,9 +0,0 @@
-package com.company.epam.thirdtask;
-
-public class TaskView {
-
-    public void showResult(int perimeter, int area) {
-        System.out.println("Периметр треугольника равен " +perimeter + "\nПлощадь треугольника равна " +area);
-
-    }
-}
